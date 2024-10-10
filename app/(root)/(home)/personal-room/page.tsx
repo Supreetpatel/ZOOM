de2d2f4947c3c194f8b616523afbe07d4878e438
Supreet0 <150177964+Supreetpatel@users.noreply.github.com>
2024-10-10 19:43:19 +0530

    First

---
+++ app/(root)/(home)/personal-room/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PersonalRoom = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+        <h1 className="font-bold text-3xl">Personal</h1>
+    </section>
+  )
+}
+
+export default PersonalRoom
---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Recordings = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+        <h1 className="font-bold text-3xl">Recording</h1>
+    </section>
+  )
+}
+
+export default Recordings
